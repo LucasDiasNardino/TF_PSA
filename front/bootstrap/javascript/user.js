d2b6890ad3fac241dd1d92b27978357bcf8a96b5
lucas nardino <lucas.diasnardino@gmail.com>
2023-11-23 02:08:14 -0300

    lista de reembolsos prontos

--- front/bootstrap/javascript/user.js
+++ front/bootstrap/javascript/user.js
@@ -17,3 +17,55 @@ function atualizarEstadoBotao() {
 
     enviarBut.disabled = !(valorPreenchido && descricaoPreenchido);
 }
+
+
+
+/*
+*  Busca os reembolsos no banco e adiciona na lista
+*/
+
+function atualizarLista() {
+    fetch('http://localhost:8080/reembolso/listar', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+            // Adicione outros cabeçalhos conforme necessário
+        },
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao obter reembolsos');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Limpar a lista existente
+        var listaReembolsos = document.getElementById('reembolsos');
+        listaReembolsos.innerHTML = '';
+
+        // Iterar sobre os reembolsos e adicionar elementos à lista
+        data.forEach(reembolso => {
+            // Adicionar apenas os campos desejados à lista
+            var li = document.createElement('li');
+            li.textContent = `Descrição: ${reembolso.descricao}, Data: ${reembolso.data}, Estado: ${reembolso.estado}`;
+            li.classList.add('list-group-item');
+            listaReembolsos.appendChild(li);
+        });
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Chamar a função inicialmente e a cada 5 segundos
+    atualizarLista();
+    setInterval(atualizarLista, 5000);
+
+    // Adicionar um evento de clique ao botão
+    var btnAtualizar = document.getElementById('atualizarBtn');
+    btnAtualizar.addEventListener('click', function() {
+        // Chamar a função ao clicar no botão
+        atualizarLista();
+    });
+});
